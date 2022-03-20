932b5a2f429a910920c2b058abc9f65b2f9ed0ed
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-20 20:32:40 +0000

    formatted code

--- example.py
+++ example.py
@@ -2,11 +2,12 @@ import tkinter as tk
 from tkinter import *
 from tkinter_webcam import webcam
 
-window = tk.Tk() # Sets up GUI
-window.title("Example") # Titles GUI
-window.geometry("1000x1000") # Sizes GUI
+window = tk.Tk()  # Sets up GUI
+window.title("Example")  # Titles GUI
+window.geometry("1000x1000")  # Sizes GUI
 
-video = webcam.Box(window, 450, 450) # Uses Box class from webcam to create video window
-video.show_frames() # Show the created Box
+# Uses Box class from webcam to create video window
+video = webcam.Box(window, 450, 450)
+video.show_frames()  # Show the created Box
 
 tk.mainloop()
--- tkinter_webcam/webcam.py
+++ tkinter_webcam/webcam.py
@@ -1,26 +1,29 @@
 import tkinter as tk
 from tkinter import *
-import cv2 
+import cv2
 from PIL import Image, ImageTk
+
 
 class Box:
     def __init__(self, window, width, height):
         self.window = window
         self.width = width
         self.height = height
-        
+
         self.label = Label(self.window, width=self.width, height=self.height)
         self.cap = cv2.VideoCapture(0)
         self.label.pack()
 
-    def show_frames(self): # Define function to show frame
+    def show_frames(self):  # Define function to show frame
 
-        cv2image= cv2.cvtColor(self.cap.read()[1],cv2.COLOR_BGR2RGB) # Get the latest frame and convert into Image
+        # Get the latest frame and convert into Image
+        cv2image = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)
         img = Image.fromarray(cv2image)
-        
-        imgtk = ImageTk.PhotoImage(image = img) # Convert image to PhotoImage
+
+        imgtk = ImageTk.PhotoImage(image=img)  # Convert image to PhotoImage
 
         self.label.imgtk = imgtk
         self.label.configure(image=imgtk)
-        
-        self.label.after(20, self.show_frames) # Repeat after an interval to capture continiously
+
+        # Repeat after an interval to capture continiously
+        self.label.after(20, self.show_frames)
